06c8fdb2cce80fac45e21672500390b6ff229127
Avinash Kumar <avinash.k95347@gmail.com>
2025-01-15 02:40:47 +0530

    fix build erroors

--- src/components/RouteLoder/ProgressBar.tsx
+++ src/components/RouteLoder/ProgressBar.tsx
@@ -1,20 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useNavigation, useLocation } from 'react-router-dom';
 import LoadingBar from 'react-top-loading-bar';
+
 
 const ProgressBar = () => {
   const [progress, setProgress] = useState(0);
+  const navigation = useNavigation();
   const location = useLocation();
 
   useEffect(() => {
-    setProgress(30); // Start loading
+    if (navigation.state === 'loading') {
+      setProgress(30); // Start loading
+    }
 
-    const timer = setTimeout(() => {
+    if (navigation.state === 'idle') {
       setProgress(100); // Finish loading
-    }, 500); // Simulate loading delay
-
-    return () => clearTimeout(timer);
-  }, [location]);
+    }
+  }, [navigation.state, location.pathname]);
 
   return (
     <div style={{ marginTop: '60px' }}>
